5a0c976fa4314d6926db279500dfd548eb1837c6
Dariusz Walczak <dariusz.walczak@gmail.com>
2020-01-11 15:59:39 +0100

    Few tweaks and extensions in the cjm module.
    * Added new common cli argument.
    * Added 'make_cj_request' function.
    * Tweaked 'make_cj_url' and 'make_cj_agile_url' functions

--- script/cjm/__init__.py
+++ script/cjm/__init__.py
@@ -5,14 +5,22 @@ import sys
 import urllib.parse
 
 # Third party imports
+import requests
 import simplejson
 
+
+USER_NAME_ARG_NAME = "--user"
+USER_TOKEN_ARG_NAME = "--token"
 
 def fmt_dft(v):
     return "" if v is None else " (default: {0})".format(v)
 
 
 def make_common_parser(defaults):
+    default_data_path = os.path.relpath(
+        os.path.join(
+            os.path.realpath(__file__),
+            "..", "..", "..", "data"))
     default_user_name = defaults.get("jira", {}).get("user", {}).get("name", None)
     default_user_token = defaults.get("jira", {}).get("user", {}).get("token", None)
     default_jira_host = defaults.get("jira", {}).get("host", "openness.atlassian.net")
@@ -20,18 +28,23 @@ def make_common_parser(defaults):
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--user", action="store", metavar="NAME", dest="user_name", default=default_user_name,
+        USER_NAME_ARG_NAME, action="store", metavar="NAME", dest="user_name",
+        default=default_user_name,
         help=(
             "User NAME to be used to access the cloud Jira API{0:s}"
             "".format(fmt_dft(default_user_name))))
     parser.add_argument(
-        "--token", action="store", metavar="VALUE", dest="user_token", default=default_user_token,
+        USER_TOKEN_ARG_NAME, action="store", metavar="VALUE", dest="user_token",
+        default=default_user_token,
         help=(
             "User token to be used to access the cloud Jira API{0:s}"
             "".format(fmt_dft(default_user_token))))
     parser.add_argument(
         "--json-output", action="store_true", dest="json_output",
         help="Print the results serialized to JSON")
+    parser.add_argument(
+        "--data-dir", action="store", metavar="PATH", dest="data_dir_path",
+        help="Toolchain data directory PATH (default: '{0:s}')".format(default_data_path))
 
     return parser
 
@@ -58,15 +71,40 @@ def load_defaults(file_name=DEFAULTS_FILE_NAME):
     return defaults
 
 
-def make_cj_url(cfg, resource_path):
+def make_cj_url(cfg, *resource_path):
     url_parts = (
-        cfg["jira"]["scheme"], cfg["jira"]["host"], "/".join((_CJ_API_PATH, resource_path)),
+        cfg["jira"]["scheme"], cfg["jira"]["host"], "/".join((_CJ_API_PATH, *resource_path)),
         "", "", "")
     return urllib.parse.urlunparse(url_parts)
 
 
-def make_cj_agile_url(cfg, resource_path):
+def make_cj_agile_url(cfg, *resource_path):
     url_parts = (
-        cfg["jira"]["scheme"], cfg["jira"]["host"], "/".join((_CJ_AGILE_PATH, resource_path)),
+        cfg["jira"]["scheme"], cfg["jira"]["host"], "/".join((_CJ_AGILE_PATH, *resource_path)),
         "", "", "")
     return urllib.parse.urlunparse(url_parts)
+
+
+def make_cj_request(cfg, url):
+    if cfg["jira"]["user"]["name"] is None:
+        sys.stderr.write(
+            "ERROR: Jira user name not specified. Use the '{0:s}' CLI option or the defaults"
+            " file to specify it\n".format(USER_NAME_ARG_NAME))
+        return (1, None)
+
+    if cfg["jira"]["user"]["token"] is None:
+        sys.stderr.write(
+            "ERROR: Jira user token not specified. Use the '{0:s}' CLI option or the defaults"
+            " file to specify it\n".format(USER_TOKEN_ARG_NAME))
+        return (1, None)
+
+    response = requests.get(
+        url, auth=(cfg["jira"]["user"]["name"], cfg["jira"]["user"]["token"]))
+
+    if response.status_code != 200:
+        sys.stderr.write(
+            "ERROR: The Jira API request ('{0:s}') failed with code {1:d}\n"
+            "".format(url, response.status_code))
+        return (2, response)
+
+    return (0, response)
