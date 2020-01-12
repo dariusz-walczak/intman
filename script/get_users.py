e6cbf640749d0e3253feb54ccb319a31eca320f9
Jakub Rymsza <jakub.rymsza@mobica.com>
2020-01-12 16:43:50 -0500

    Parsing of users' data added

--- script/get_users.py
+++ script/get_users.py
@@ -13,17 +13,37 @@ def main(options):
     auth = HTTPBasicAuth("email@example.com", "<api_token>")
 
     headers = {
-       "Accept": "application/json"
+        "Accept": "application/json"
     }
 
     response = requests.request(
-       "GET",
-       url,
-       headers=headers,
-       auth=auth
+        "GET",
+        url,
+        headers=headers,
+        auth=auth
     )
 
-    print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
+    all_users_data = json.loads(response.text)
+    human_users = [user for user in all_users_data if user["accountType"] == "atlassian"]
+
+    people = list()
+    for user in human_users:
+        if str(user["displayName"]).find(' ') != -1:
+            firstname, lastname = str(user["displayName"]).split(" ", 1)
+        elif str(user["displayName"]).find('.') != -1:
+            firstname, lastname = str(user["displayName"]).split(".", 1)
+        elif str(user["displayName"]).find(',') != -1:
+            firstname, lastname = str(user["displayName"]).split(",", 1)
+        else:
+            firstname = user["displayName"]
+            lastname = "LASTNAME"
+
+        acc_id_last_3_digs = "".join(list(user["accountId"])[-3:])
+        code = str(firstname[0] + lastname[0] + "." + acc_id_last_3_digs).upper()
+
+        people.append({"code": code, "first name": firstname, "last name": lastname})
+
+    print(json.dumps(people, sort_keys=True, indent=4, separators=(",", ": ")))
 
 
 def parse_options(args):
