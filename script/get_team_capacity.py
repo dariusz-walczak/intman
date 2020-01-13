3772ac63eda7ff49135111f41ed130f45e479591
Jakub Rymsza <jakub.rymsza@mobica.com>
2020-01-13 09:58:31 -0500

    Added writing data to JSON file, amended stored data.

--- script/get_team_capacity.py
+++ script/get_team_capacity.py
@@ -8,9 +8,13 @@ import argparse
 
 
 def main(options):
+    out_file = "../data/team_data.json"
+    pretty_out_file = "../data/team_data_pretty.json"
+
     url = "https://openness.atlassian.net/rest/api/3/users"
 
-    auth = HTTPBasicAuth("email@example.com", "<api_token>")
+    # REMOVE BEFORE PUSH - REMOVE BEFORE PUSH - REMOVE BEFORE PUSH - REMOVE BEFORE PUSH - REMOVE BEFORE PUSH
+    auth = HTTPBasicAuth("jakubx.rymsza@intel.com", "9nvcRB8xKzJdxKlrf3vd3F0F")
 
     headers = {
         "Accept": "application/json"
@@ -26,7 +30,7 @@ def main(options):
     all_users_data = json.loads(response.text)
     human_users = [user for user in all_users_data if user["accountType"] == "atlassian"]
 
-    people = list()
+    users = list()
     for user in human_users:
         if str(user["displayName"]).find(' ') != -1:
             firstname, lastname = str(user["displayName"]).split(" ", 1)
@@ -41,9 +45,28 @@ def main(options):
         acc_id_last_3_digs = "".join(list(user["accountId"])[-3:])
         code = str(firstname[0] + lastname[0] + "." + acc_id_last_3_digs).upper()
 
-        people.append({"code": code, "first name": firstname, "last name": lastname})
+        users.append({"code": code, "last name": lastname, "first name": firstname, "user name": ""})
 
-    print(json.dumps(people, sort_keys=True, indent=4, separators=(",", ": ")))
+    people = dict()
+    people.update({"people": users})
+
+    people_json = json.loads(json.dumps(people))
+
+    try:
+        with open(out_file, "w") as file_object:
+            json.dump(people_json, file_object)
+            print(f"{out_file} created.")
+    except FileNotFoundError:
+        print(f"Error writing file {out_file}.")
+
+    try:
+        with open(pretty_out_file, "w") as pretty_file_object:
+            json.dump(people_json, pretty_file_object, indent=4, separators=(',', ': '))
+            print(f"{pretty_out_file} created.")
+    except FileNotFoundError:
+        print(f"Error writing file {pretty_out_file}.")
+
+    return 0
 
 
 def parse_options(args):
