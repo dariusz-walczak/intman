e0a714f5120e9269800e9e1ce3e75f71418d82cc
Dariusz Walczak <dariusz.walczak@mobica.com>
2020-01-23 19:46:06 +0100

    Fixed sprint name for one week sprint

--- script/cjm/sprint.py
+++ script/cjm/sprint.py
@@ -14,7 +14,10 @@ def generate_sprint_name(project_name, start_dt, end_dt):
     _, start_ww, _ = start_dt.isocalendar()
     _, end_ww, _ = end_dt.isocalendar()
 
-    return "{0:s} WW{1:02d}-WW{2:02d}".format(project_name, start_ww, end_ww)
+    if start_ww != end_ww:
+        return "{0:s} WW{1:02d}-WW{2:02d}".format(project_name, start_ww, end_ww)
+    else:
+        return "{0:s} WW{1:02d}".format(project_name, start_ww)
 
 
 def load_data(cfg, sprint_file):
