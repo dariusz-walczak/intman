35c6ce625128e79c17bf225e731e43a46f94949c
Tomasz Wesolowski <tomasz.wesolowski@mobica.com>
2020-02-19 12:05:51 +0100

    dry run initial implementation

--- script/cjm/__init__.py
+++ script/cjm/__init__.py
@@ -1,3 +1,4 @@
 SM_CREATE_COMMITMENT_FILE = "sm-create-commitment-file.py"
 SM_CREATE_SPRINT_FILE = "sm-create-sprint-file.py"
 SM_CREATE_TEAM_FILE = "sm-create-team-file.py"
+SM_CREATE_GENERATE_DELIVERY_REPORT = "sm-generate-delivery-report.py"
