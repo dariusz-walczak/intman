b47d3a29eac1badabf0dc86e2159be09f95c2ae7
Dariusz Walczak <dariusz.walczak@mobica.com>
2020-02-29 11:02:23 +0100

    Minor formatting change

--- script/cjm/codes.py
+++ script/cjm/codes.py
@@ -2,7 +2,7 @@ NO_ERROR            = 0
 CONFIGURATION_ERROR = 1
 REQUEST_ERROR       = 2
 FILESYSTEM_ERROR    = 3
-INTEGRATION_ERROR   = 4  # Indicates that some assumption about how the Jira works seems to be false
+INTEGRATION_ERROR   = 4 # Indicates that some assumption about how the Jira works seems to be false
 INVALID_ARGUMENT_ERROR = 5
 
 class CjmError(Exception):
