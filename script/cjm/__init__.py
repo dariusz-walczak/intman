c28bd59956ca619a95d1c699a8dcde4f71e53f0b
Dariusz Walczak <dariusz.walczak@mobica.com>
2020-04-30 17:24:24 +0200

    Added first version of the 'sm-create-tasks-file' script

--- script/cjm/__init__.py
+++ script/cjm/__init__.py
@@ -3,5 +3,6 @@
 SM_CREATE_CAPACITY_FILE = "sm-create-capacity-file.py"
 SM_CREATE_COMMITMENT_FILE = "sm-create-commitment-file.py"
 SM_CREATE_SPRINT_FILE = "sm-create-sprint-file.py"
+SM_CREATE_TASKS_FILE = "sm-create-tasks-file.py"
 SM_CREATE_TEAM_FILE = "sm-create-team-file.py"
 SM_CREATE_GENERATE_DELIVERY_REPORT = "sm-generate-delivery-report.py"
---
+++ script/sm-create-tasks-file.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""Command line script pushing task list into jira"""
+
+# Standard library imports
+import datetime
+import json
+import secrets
+import string
+import sys
+
+# Third party imports
+import jsonschema
+
+# Project imports
+import cjm.cfg
+import cjm.run
+import cjm.schema
+
+
+def parse_options(args):
+    """Parse command line options"""
+    defaults = cjm.cfg.load_defaults()
+    parser = cjm.cfg.make_common_parser(defaults)
+
+    return parser.parse_args(args)
+
+
+def main(options):
+    """Entry function"""
+    cfg = cjm.cfg.apply_options(cjm.cfg.init_defaults(), options)
+
+    tasks_json = {
+        "set id": secrets.token_hex(16),
+        "author": cfg["jira"]["user"]["name"],
+        "date": datetime.date.today().isoformat(),
+        "tasks": []
+    }
+
+    tasks_schema = cjm.schema.load(cfg, "tasks.json")
+    jsonschema.validate(tasks_json, tasks_schema)
+
+    print(json.dumps(tasks_json, indent=4, sort_keys=False))
+
+    return cjm.codes.NO_ERROR
+
+
+if __name__ == '__main__':
+    cjm.run.run(main, parse_options(sys.argv[1:]))
